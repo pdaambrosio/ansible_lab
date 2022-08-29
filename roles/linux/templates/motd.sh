d0f59c0a0fe267eede3ce9c9d1286b2f1de49f12
Paulo Daniel A Santos <pda.ambrosio@gmail.com>
2022-08-29 08:32:49 -0300

    feature: init

---
+++ roles/linux/templates/motd.sh
@@ -0,0 +1,82 @@
+#!/bin/sh
+
+echo "
+------------------------------------------------------------------------------------------
+                                        Welcome to Hell
+
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⢀⣤⣶⠶⠿⠿⠿⠿⠿⠿⠿⠿⠿⢿⣿⠿⠿⠿⠷⠶⠶⢶⣶⣶⣶⣤⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⢀⣶⠟⠁⠀⠀⠀⠀⣀⠤⠒⠉⣁⣀⠤⠤⠤⠤⠤⠄⣀⣀⡉⠉⠉⠉⠉⢉⣉⣉⣉⣉⣛⠛⢷⣦⣄⠀⠀⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⣠⡿⠁⠀⠀⢀⠤⠚⣁⠔⣚⡥⠐⠒⠒⠒⠒⠒⠲⡂⠀⠀⠀⠀⠀⠀⣀⡤⠤⠤⠤⠤⣄⠀⠀⠀⠀⠙⢿⣦⠀⠀⠀⠀
+    ⠀⠀⠀⠀⠀⣴⡟⠀⠀⠀⠈⠀⠤⢉⠔⠉⠀⠀⢀⣀⣀⣀⠀⠀⠀⠹⠀⠀⠀⠀⠀⠀⣠⠀⠀⠀⠀⠀⠀⠈⢦⠀⠀⠀⠀⣿⡄⠀⠀⠀
+    ⠀⠀⠀⣠⣾⠟⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⢿⣿⣿⣿⣿⠉⠛⠻⣶⣄⠀⠀⠀⠀⠀⠀⠘⣀⣤⣤⣶⣶⣦⣤⠀⠀⠀⠀⠀⠘⣿⣤⠀⠀
+    ⠀⣤⡿⡯⠋⠉⣉⣭⣤⣤⣄⠉⠡⠀⠛⠛⠛⠛⢉⣽⠛⠛⠿⣶⣤⣿⠃⠀⠀⠀⠲⣶⣿⣿⠿⠛⠛⠛⠛⠛⠁⠉⠉⠉⠉⡙⠦⡉⢿⣦
+    ⣾⠋⣼⠀⣠⡿⠋⠀⢠⣄⠉⠛⠻⢷⣶⣶⣶⠿⠛⠀⠀⠀⠀⠀⠈⠀⠀⠀⠀⠀⠀⠀⣿⠀⠀⠀⠀⣠⣄⣀⣤⣶⠿⠿⢷⣤⢻⠀⡆⣿
+    ⣿⠀⣿⠀⣿⠀⣀⣤⣿⠛⢿⣶⣤⣀⠀⠀⠀⠀⢀⣀⣀⣀⠆⣤⣶⣦⠀⠀⠀⠀⠀⠀⠙⠻⣶⣄⠀⠀⠉⠉⠉⠀⣾⠀⠀⠀⣸⠀⢇⣿
+    ⣿⣄⠹⠀⢻⣇⠀⠀⢻⣷⡀⠀⠈⠛⣿⣷⣶⣤⣄⣀⠀⠀⠘⣿⡄⢶⠷⠶⠶⠀⠀⠀⠀⣤⡿⠛⠛⠒⠤⡀⠀⣠⣿⣿⠀⣀⠤⢚⣿⠋
+    ⠀⠻⣷⣌⠓⠀⠀⠀⠀⠻⣿⠿⣶⣤⣿⣦⠀⠀⠉⠉⠛⢿⣷⣶⣤⣤⣄⣀⣀⢀⠙⠛⠛⠉⠀⠀⣀⣠⣤⣶⣿⡟⣿⣿⡆⠀⠀⣿⠃⠀
+    ⠀⠀⠈⠻⣷⡀⠀⠀⠀⠀⠉⢿⣄⠈⣿⡿⣿⣿⣶⣦⣤⣿⡇⠀⠀⠀⠀⠙⣿⠋⠛⠛⢻⣿⠛⠛⠉⢻⣧⠀⣸⣧⣼⣿⡇⠀⢸⣿⠀⠀
+    ⠀⠀⠀⠀⠉⣿⡄⠀⠀⠀⠀⠀⠙⣷⣿⠁⠀⠀⠉⠙⢻⣿⢿⣿⣿⣷⣶⣶⣿⣶⣶⣶⣾⣿⣶⣶⣷⣿⣿⣿⣿⣿⣿⣿⠇⠀⢸⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠈⢿⣦⠀⠀⠀⠀⠀⠀⠉⠻⣶⣄⠀⠀⣿⠏⠀⠀⠀⠉⠙⣿⠛⠛⠿⠿⣿⣿⣿⣿⣿⣿⠿⣿⠟⣿⢯⣿⠀⠀⢸⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠉⠻⣦⣀⠢⣄⠀⠀⣄⠀⠉⠛⠿⣿⣤⣄⣀⠀⠀⢀⣿⠀⠀⠀⢀⣿⠀⠀⣴⡿⠀⣾⣏⣤⣿⠿⠁⠀⠀⢸⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠛⠿⣶⣤⡉⠒⢤⣉⠒⠤⣄⠀⠀⠉⠉⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠛⠋⠉⠁⢀⠀⠀⠀⠀⠈⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣶⣤⣄⠉⠒⠠⠭⣁⣒⠠⠤⠤⠤⣄⣀⣀⣀⣀⣀⣀⣀⣀⣠⠤⠒⠁⠀⣀⠞⠀⠀⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠉⠛⠿⣶⣤⣀⠀⠀⠈⠉⠉⠀⠒⠒⠒⠀⠀⠐⠀⠠⠠⠠⠀⠒⠉⠀⠀⠀⣠⣿⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠙⠛⠿⣶⣶⣤⣤⣄⣀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣠⣶⠟⠀⠀⠀
+    ⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⠈⠉⠉⠛⠛⠛⠛⠓⠒⠒⠒⠒⠛⠛⠛⠉⠀⠀⠀⠀⠀⠀
+"
+echo "Hostname: {{ ansible_fqdn }}"
+echo "User:     $(whoami)"
+echo "Logged:   $(who |wc -l)"
+printf "\n"
+echo "System:   {{ ansible_distribution }}"
+echo "Version:  {{ ansible_distribution_version }}"
+echo "Kernel:   {{ ansible_kernel }}"
+printf "\n"
+
+#System date
+date=`date`
+
+#System load
+LOAD1=`cat /proc/loadavg |awk {'print $1'}`
+LOAD5=`cat /proc/loadavg |awk {'print $2'}`
+LOAD15=`cat /proc/loadavg |awk {'print $3'}`
+
+#System uptime
+uptime=`cat /proc/uptime |cut -f1 -d.`
+upDays=$((uptime/60/60/24))
+upHours=$((uptime/60/60%24))
+upMins=$((uptime/60%60))
+upSecs=$((uptime%60))
+
+#Root fs info
+root_free=`df -h / |awk '/\// {print $4}'|grep -v "^$"`
+boot_free=`df -h /boot |awk '/\// {print $4}'|grep -v "^$"`
+
+#Memory Usage
+memory_usage=`free -m |grep Mem |awk '{print $3/$2 * 100.0}' |xargs printf "%.*f\n" 2 |sed 's/$/%/'`
+swap_usage=`free -m | grep Swap | awk '{print $3/$2 * 100.0}' | xargs printf "%.*f\n" 2 | sed 's/$/%/'`
+
+#Processes
+processes=`ps aux |wc -l`
+
+#Interfaces
+INTERFACE=$(ip -4 ad |grep 'state UP' |awk -F ":" '!/^[0-9]*: ?lo/ {print $2}')
+
+echo "System information as of: $date"
+echo
+printf "System Load:\t%s %s %s\\nSystem Uptime:\t%s "days" %s "hours" %s "min" %s "sec"\n" $LOAD1, $LOAD5, $LOAD15 $upDays $upHours $upMins $upSecs
+printf "Memory Usage:\t%s\t\t\t\nSwap Usage:\t%s\n" $memory_usage $swap_usage
+printf "Free on /:\t%s\t\t\t\nFree on /boot:\t%s\n" $root_free $boot_free
+printf "Processes:\t%s\t\t\t\n" $processes
+printf "\n"
+printf "Interface\tMAC Address\t\tIP Address\t\n"
+
+for x in $INTERFACE
+  do
+        MAC=$(ip ad show dev $x |grep link/ether |awk '{print $2}')
+        IP=$(ip ad show dev $x |grep -v inet6 | grep inet|awk '{print $2}')
+        printf  $x"\t\t"$MAC"\t"$IP"\t\n"
+
+  done
+echo
+echo "------------------------------------------------------------------------------------------"
